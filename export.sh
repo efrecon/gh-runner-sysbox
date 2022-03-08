5d1d6754d1a7a6ad8c796a8a975b192902a0a123
Emmanuel Frécon <efrecon@gmail.com>
2022-03-08 12:00:34 +0100

    Capture environment from PID 1

--- export.sh
+++ export.sh
@@ -10,14 +10,21 @@ set -eu
 # export.
 EXPORT_PREVENT=${EXPORT_PREVENT:-"CWD|HOME|HOSTNAME|KUBERNETES_.*|LANG|LS_COLORS|PATH|PWD|SHELL|SSH_.*|TERM|USER|XDG_.*|_"}
 
+# Name of the user to give the file for access. The file will be restricted for
+# read/write by this user only.
 EXPORT_USER=${EXPORT_USER:-"$(id -nu)"}
 
-while getopts "x:u:h-" opt; do
+# PID to get environment from, empty means current environment instead
+EXPORT_PID=${EXPORT_PID:-""}
+
+while getopts "x:u:p:h-" opt; do
   case "$opt" in
     x) # Exclusion list
       EXPORT_PREVENT=$OPTARG;;
     u) # User to give away file to
       EXPORT_USER=$OPTARG;;
+    p) # PID to get environment from
+      EXPORT_PID=$OPTARG;;
     h) # Print help and exit
       echo "Export relevant environment to conf file";;
     -)
@@ -28,13 +35,21 @@ while getopts "x:u:h-" opt; do
 done
 shift $((OPTIND-1))
 
+dumpenv() {
+  if [ -z "$EXPORT_PID" ]; then
+    env
+  else
+    xargs -0 -l1 echo < "/proc/${EXPORT_PID}/environ"
+  fi
+}
+
 # Create directory to file, if necessary
 if [ "$#" -gt "0" ]; then
   mkdir -p "$(dirname "$1")"
 fi
 
 # Export selected part of the environment
-env | grep -E -e '^[A-Z_]+=' | grep -E -v "^(${EXPORT_PREVENT})=" > "${1:-/dev/stdout}"
+dumpenv | grep -E -e '^[A-Z_]+=' | grep -E -v "^(${EXPORT_PREVENT})=" > "${1:-/dev/stdout}"
 
 # Prevent access to file
 if [ "$#" -gt "0" ]; then
